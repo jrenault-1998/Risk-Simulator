9b7a41ed7e4afeea882d23a1a2687e4910d5ce4f
jrenault <joshjrenault@gmail.com>
2022-09-14 20:04:17 -0400

    Ideas for class and object comments

---
+++ Classes.py
@@ -0,0 +1,10 @@
+##Class Player
+# "Player" owns the country
+## player makes a playerMove which consists of attacking another country
+
+
+#Superclass Continents
+
+## 'sub'Class Country
+### class has colourOccupying, numOfTroops, nearbyCountries
+
